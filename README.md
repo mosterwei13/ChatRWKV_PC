6bc6098578b2370d318969936c03374bd7b9cac5
ZG <673036325@qq.com>
2023-04-26 09:25:59 +0800

    初始化仓库

---
+++ README.md
@@ -0,0 +1,46 @@
+# ChatRWKV桌面版懒人包
+![图片alt](/20230425182102.png)
+***
+## 说明
+
+本懒人包基于ChatRWKV制作,该项目地址：[https://github.com/BlinkDL/ChatRWKV](https://github.com/BlinkDL/ChatRWKV)
+
+UI使用HandyControl控件库 [开发文档地址](https://handyorg.github.io/handycontrol/)、[GitHub地址](https://github.com/HandyOrg/HandyControl)
+
+之前在知乎发布的版本部分代码是有授权限制的,花了点时间现移除了授权部分的代码,所以UI会和之前不同,功能上并无差别。
+
+先声明当初是想着自己用的,注释什么的基本算没有吧
+
+***
+## 编译环境要求
+
+1.Net6.0
+
+2.Visual Studio Community 2022/2019
+
+3.VS安装.Net桌面开发
+
+4.Windows11/10
+
+***
+## 打包程序步骤
+1.双击ChatRWKV_PC.sln打开解决方案
+
+2.右键ChatRWKV_PC项目,选择发布
+
+3.添加发布配置文件
+
+4.目标和特定目标都选择文件夹,其它选项默认
+
+5.自行选择其它设置项
+
+6.发布
+
+***
+## 协议说明
+Apache License 2.0
+
+## 觉得不错的话
+
+![图片alt](/20230425224023.png)
+![图片alt](/20230425225459.png)
---
+++ Utils/UpdateUtil.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+
+namespace ChatRWKV_PC.Utils
+{
+    public class UpdateUtil
+    {
+        public async void CheckUpdateVersion(string Name)
+        {
+            string currentVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            int currentVersionInt = int.Parse(currentVersion.Replace(".", ""));
+            try
+            {
+                using (var client = new HttpClient())
+                {
+
+                    var response = await client.GetAsync("https://gitee.com/api/v5/repos/{owner}/{repo}/contents(/{path}?access_token={you_access_token}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = response.Content.ReadAsStringAsync().Result;
+                        try
+                        {
+                            JObject obj = (JObject)JsonConvert.DeserializeObject(content);
+                            if (obj != null)
+                            {
+
+                                string info = Encoding.UTF8.GetString(
+                                    Convert.FromBase64String(
+                                        obj.GetValue("content").ToString()
+                                        )
+                                    );
+                                info = info.Replace("\n", "");
+                                info = info.Replace(" ", "");
+
+                                List<JObject> listObj = JsonConvert.DeserializeObject<List<JObject>>(info);
+
+                                if (listObj != null)
+                                {
+                                    foreach (JObject softObj in listObj)
+                                    {
+                                        string softName = softObj.GetValue("Name").ToString();
+                                        if (softName.Equals(Name))
+                                        {
+                                            string lastVersion = softObj.GetValue("LastVersion").ToString();
+                                            int lastVersionInt = int.Parse(lastVersion.Replace(".", ""));
+
+                                            if (currentVersionInt >= lastVersionInt)
+                                            {
+                                                return;
+                                            }
+                                            else
+                                            {
+                                                string update = softObj.GetValue("Update").ToString();
+                                                string desc = softObj.GetValue("Desc").ToString();
+                                                string msg = string.Format("{0}有更新，本窗口只做提示，请自行更新\n版本：{1}->{2}\n更新时间：{3}\n更新说明：\n{4}", Name, currentVersion, lastVersion, update, desc);
+                                                MessageBox.Show(msg,"有新版本了！");
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error getting file: " + response.ReasonPhrase);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
